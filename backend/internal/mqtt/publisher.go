afb004816e114a21c615c6ffc3188c06d1cfac94
EmirMurat <150227975+s0231527@users.noreply.github.com>
2026-04-06 16:37:09 +0200

    Project structure extended

---
+++ backend/internal/mqtt/publisher.go
@@ -0,0 +1,8 @@
+// Package mqtt is responsible for publishing (train) trip data to the MQTT broker
+//
+// HiveMQ is used to host a broker that can ease communication between
+// both publishers and subscribers.
+package mqtt
+
+
+
