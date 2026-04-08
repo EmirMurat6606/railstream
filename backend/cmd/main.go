8ece7c9d527c664a0f4888e9e9bc822111f3e0f7
EmirMurat <150227975+s0231527@users.noreply.github.com>
2026-04-08 02:01:17 +0200

    MQTT publisher setup

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,11 +2,22 @@
 package main
 
 import (
-	"fmt"
+	pub "github.com/EmirMurat6606/railstream/internal/mqtt"
 )
 
+func check(err error){
+	if (err != nil){
+		panic(err)
+	}
+}
 
-func main(){
-	fmt.Print("Hello World")
+func main() {
+	publisher, err := pub.NewMqttPublisher("49970051230a4dacb5b34fe2e2447647.s1.eu.hivemq.cloud", 8883, "railstreamer", ".env")
+	check(err)
+	publisher.Print()
+	var loop bool = true
 
+	for (loop) {
+	
+	}
 }
--- backend/internal/mqtt/publisher.go
+++ backend/internal/mqtt/publisher.go
@@ -4,44 +4,91 @@
 // both publishers and subscribers.
 package mqtt
 
-
-import(
+import (
 	"fmt"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	env "github.com/joho/godotenv"
 )
-
 
 // this callback triggers when a message is received, it then prints the message (in the payload) and topic
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-  fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
 }
 
 // upon connection to the client, this is called
 var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-  fmt.Println("Connected")
+	fmt.Println("Connected")
 }
 
 // this is called when the connection to the client is lost, it prints "Connection lost" and the corresponding error
 var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-  fmt.Printf("Connection lost: %v", err)
+	fmt.Printf("Connection lost: %v", err)
 }
 
-type credentials struct{
+type credentials struct {
 	Username string
 	Password string
 }
 
-func newCredentials(path string) *credentials{
+func loadCredentials(path string) (*credentials, error) {
 	// Read credentials from the path
+	data, err := env.Read(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(data)
+
+	var credentials = credentials{
+		Username: data["MQTT_USERNAME"],
+		Password: data["MQTT_PASSWORD"],
+	}
+
+	return &credentials, nil
+}
+
+type publisher struct {
+	client mqtt.Client
+}
+
+// NewMqttPublisher creates a new mqttPublisher object
+func NewMqttPublisher(broker string, port uint16, clientName string, credentialsPath string) (*publisher, error) {
+
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
+	opts.SetClientID(clientName)
+
+	if len(credentialsPath) != 0 {
+		credentials, err := loadCredentials(credentialsPath)
+
+		if err != nil {
+			return nil, err
+		}
+
+		opts.SetUsername(credentials.Username)
+		opts.SetPassword(credentials.Password)
+	}
+
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	var publisher = publisher{
+		client: mqtt.NewClient(opts),
+	}
+
+	// return an error if the connection isn't successfull
+	if token := publisher.client.Connect(); token.Wait() && token.Error() != nil {
+		return nil, token.Error()
+	}
+
+	return &publisher, nil
+}
+
+func (*publisher) Print() error {
+
+	fmt.Println("hello, I am printing")
 	return nil
 }
-
-type MqttPublisher struct{
-
-	Broker string
-	Credentials credentials
-
-}
-
-
